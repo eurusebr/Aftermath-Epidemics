dc4947b173865490b4a0122907c11e5c51ac8842
Mahdieh Ebrahimi <evrusebr@gmail.com>
2022-01-09 14:05:21 +0330

    prototype 4

--- Levy.cpp
+++ Levy.cpp
@@ -34,36 +34,44 @@
 using namespace std;
 
 
-vector<vector<double>> levy(int size, double C, mt19937& gen, mt19937& genn, uniform_real_distribution<>& distr);
+vector<int> levy(int size, double C, mt19937& gen, mt19937& genn, uniform_real_distribution<>& distr);
 vector<vector<int>> split (const std::vector<int>& v); 
 vector<int> Divisors(int n);
-vector<int> path_maker(vector<vector<double>>& total);
 vector<vector<double>> find_clusters(int ii, int const& num_iter, vector<int> indexes, int PBS_Pos);
 
 
 int main(void)
 {
-	srand(time(0));
-	// random_device rd;
-	// mt19937 gen(rd()); //Mersenne Twister RNG
-	// int ff = rand();
+	vector<double> final_delta;
+	int ff = rand();
+	ofstream output;
+	output.open("C:/Users/mohak/Desktop/Levy-flight/C++/result/Result_" + to_string(Lx) + "_" + to_string(ff) + ".txt");
 	
 	for (int k = 0; k < iteration; k++)
 	{
+		printf("%d \n", k);
 		vector<vector<double>> total;
 		vector<int> indexes, divisions;
 		vector<float> final_total_landmass, final_big_cluster;
 		uniform_real_distribution<> distr(0, 1);
+		vector<double> delta;
+		// vector<double> r;
 		// normal_distribution<> d{0, (1/pow(2*C, 0.5))}; //the first entery is mean and the second is the std
 		random_device rd, rand;
 		mt19937 gen(rd()), genn(rand()); //Mersenne Twister RNG
 
 		double C = Levy_Alpha;
-		total = levy(steps, C, gen, genn, distr);
-		indexes = path_maker(total);
+		indexes = levy(steps, C, gen, genn, distr);
+		// for (int q = 0; q < total.size(); q++)
+		// {
+		// 	r.push_back(total[q][1]);
+		// }
+		
+		// indexes = path_maker(total);
+		// cout<<"index: "<<indexes.size()<<endl;
 		divisions = Divisors(indexes.size());
 		// int num_iter = divisions[ceil(divisions.size()/2)];
-		// int num_proc = int(steps/num_iter);
+		// int num_proc = int(indexes.size()/num_iter);
 		int num_iter = steps;
 		int num_proc = 1;
 
@@ -74,66 +82,113 @@ int main(void)
     	int PBS_counter = 0; //two;
     	int PBS_Pos = PBS_counter*PBS_index[0].size();
 
-
-		// future<vector<double>> futuer;
-		// vector<vector<vector<double>>> futures;
-
+		
 		for (int j = 0; j< num_proc; j++)//num_proc
 		{
 			std::future<vector<vector<double>>> futu = std::async(std::launch::async, find_clusters, j, num_iter, indexes, PBS_Pos);
 			auto results = futu.get();
 			std::copy(results[0].begin(), results[0].end(),  back_inserter(final_total_landmass));
 			std::copy(results[1].begin(), results[1].end(),  back_inserter(final_big_cluster));
-			printf("%d \n", j);
+			// printf("%d \n", j);
 		}
+
+		for (int i = 0; i < final_big_cluster.size()-1; i++)
+		{
+			delta.push_back(final_big_cluster[i+1] - final_big_cluster[i]);
+		}
+
+		double deltamax = double(*max_element(delta.begin(), delta.end())) /double(N);
+		final_delta.push_back(deltamax);
+		int index = distance(delta.begin(), max_element(delta.begin(), delta.end()));
+		// pc.push_back(final_total_landmass[index]);
+		// Sn.push_back(final_big_cluster[index]);
+		// Snn.push_back(final_big_cluster[index + 1]);
+		output<< deltamax << "\t" << final_total_landmass[index] << "\t" << final_big_cluster[index] << "\t" << final_big_cluster[index + 1] <<endl;
 		
 		//saving the data in npy format
-		string path1 = "/home/mah/Data_c++/Levy flight/Result/"; //"/share/users/m_movahed/cmb_cluster/s22_c++/"
-		string path = "/home/mah/Data_c++/Levy flight/Result/"; //"/share/users/m_movahed/cmb_cluster/s22_c++/"
-		string filename = "total_landmass_" + to_string(Lx)+ "_" + to_string(PBS_counter) + ".txt";
-		string filename1 = "big_cluster_" + to_string(Lx) + "_" + to_string(PBS_counter) + ".txt";
-		std::ofstream fout(path + filename);
-		fout.precision(17);
-		std::copy(final_total_landmass.begin(), final_total_landmass.end(),std::ostream_iterator<double>(fout, "\n"));
+		// string path1 = "C:/Users/mohak/Desktop/Levy-flight/C++/result/"; //"/share/users/m_movahed/cmb_cluster/s22_c++/"
+		// string path = "C:/Users/mohak/Desktop/Levy-flight/C++/result/"; //"/share/users/m_movahed/cmb_cluster/s22_c++/"
+		// string filename = "total_landmass_" + to_string(Lx)+ "_" + to_string(PBS_counter) + ".txt";
+		// string filename1 = "r_" + to_string(Lx) + "_" + to_string(PBS_counter) + ".txt";
+		// std::ofstream fout(path + filename);
+		// fout.precision(17);
+		// std::copy(final_total_landmass.begin(), final_total_landmass.end(),std::ostream_iterator<double>(fout, "\n"));
 
-		std::ofstream fout1(path1 + filename1);
-		fout1.precision(17);
-		std::copy(final_big_cluster.begin(), final_big_cluster.end(),std::ostream_iterator<double>(fout1, "\n"));
+		// std::ofstream fout1(path1 + filename1);
+		// fout1.precision(17);
+		// std::copy(r.begin(), r.end(),std::ostream_iterator<double>(fout1, "\n"));
 
 	}
+
+	output.close();
 	return 0;
 }
 
 
-vector<vector<double>> levy(int size, double C, mt19937& gen, mt19937& genn, uniform_real_distribution<>& distr)
+vector<int> levy(int size, double C, mt19937& gen, mt19937& genn, uniform_real_distribution<>& distr)
 {
-	vector<vector<double>> final;
-
+	// vector<vector<double>> final;
+	vector<int> matrix;
+	random_device rd;
+	mt19937 geen(rd()); //Mersenne Twister RNG
+	uniform_int_distribution<int> ran_pos(0, Lx-1);
+	int x = ran_pos(geen);
+	int y = ran_pos(geen);
+	matrix.push_back((Ly*x + y));
 
 	for ( int i = 0; i < steps; i++)
 	{
 		vector<double> result;
 		double angle = 2 * M_PI * distr(gen);
-		result.push_back(angle);
+		// result.push_back(angle);
 		// cout<<"angle: "<<angle<<endl;
 		double r = (pow(distr(genn), -1/C));
-
 		while (r > Lx || r < 1)
 		{
 			r = (pow(distr(genn), -1/C));
 		}
-		// cout<<"r: "<<r<<endl;
-		result.push_back(r);
-		double x = r * cos(angle);
-		// cout<<"x: "<<x<<endl;
-		result.push_back(x);
-		double y = r * sin(angle);
-		// cout<<"y: "<<y<<endl;
-		result.push_back(y);
-		final.push_back(result);
+
+		double dx = int(r * cos(angle));
+		double dy = int(r * sin(angle));
+
+		while (dx == 0 && dy == 0)
+		{
+			double r = (pow(distr(genn), -1/C));
+			while (r > Lx || r < 1)
+			{
+				r = (pow(distr(genn), -1/C));
+			}
+			double dx = int(r * cos(angle));
+			double dy = int(r * sin(angle));
+		}
+
+		x = x + dx;// == Lx ? 0 : int(x + total[i][2]);
+		if (x < 0)
+		{
+			x = (x % Lx + Lx) % Lx;
+		}
+		if (x > Lx-1)
+		{
+			x = x - Lx;
+		}
+		y = y + dy;// == Ly ? 0 :int(y + total[i][3]);
+		if (y < 0)
+		{
+			y = (y % Ly + Ly) % Ly;
+		}
+		if (y > Ly-1)
+		{
+			y = y - Ly;
+		}
+
+		matrix.push_back((Ly*x + y));
+		// result.push_back(r);
+		// result.push_back(dx);
+		// result.push_back(dy);
+		// final.push_back(result);
 	}
 
-	return final;
+	return matrix;
 
 }
 
@@ -168,39 +223,6 @@ vector<int> Divisors(int n)
     return v;
 }
 
-vector<int> path_maker(vector<vector<double>>& total )
-{
-	vector<int> matrix;
-	random_device rd;
-	mt19937 gen(rd()); //Mersenne Twister RNG
-	uniform_int_distribution<int> ran_pos(0, Lx-1);
-	int x = ran_pos(gen);
-	int y = ran_pos(gen);
-	matrix.push_back((Ly*x + y));
-	for (int i = 0; i < total.size(); i++)
-	{
-		x = x + int(total[i][2]);// == Lx ? 0 : int(x + total[i][2]);
-		if (x < 0)
-		{
-			x = (x % Lx + Lx) % Lx;
-		}
-		if (x > Lx-1)
-		{
-			x = x - Lx;
-		}
-		y = y + int(total[i][3]);// == Ly ? 0 :int(y + total[i][3]);
-		if (y < 0)
-		{
-			y = (y % Ly + Ly) % Ly;
-		}
-		if (y > Ly-1)
-		{
-			y = y - Ly;
-		}
-		matrix.push_back((Ly*x + y));
-	}
-	return matrix;
-}
 
 vector<vector<double>> find_clusters(int ii, int const& num_iter, vector<int> indexes, int PBS_Pos)
 {
